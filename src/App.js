f3036b058c6038b5c047c8bc44fc26ad462a4c1c
NecroBread <mariovyordanov@gmail.com>
2022-07-03 15:04:40 +0300

    Add colors

--- src/App.js
+++ src/App.js
@@ -9,16 +9,16 @@ function App() {
 					</div>
 					<div className="buttons">
 						<button id="clear" value="AC">AC</button>
-						<button id="divide" value="/">/</button>
-						<button id="multiply" value="*">x</button>
+						<button className="ligth-gray" id="divide" value="/">/</button>
+						<button className="ligth-gray" id="multiply" value="*">x</button>
 						<button id="seven" value="7">7</button>
 						<button id="eight" value="8">8</button>
 						<button id="nine" value="9">9</button>
-						<button id="subtract" value="-">-</button>
+						<button className="ligth-gray" id="subtract" value="-">-</button>
 						<button id="four" value="4">4</button>
 						<button id="five" value="5">5</button>
 						<button id="six" value="6">6</button>
-						<button id="add" value="+">+</button>
+						<button className="ligth-gray" id="add" value="+">+</button>
 						<button id="one" value="1">1</button>
 						<button id="two" value="2">2</button>
 						<button id="three" value="3">3</button>
